ad0c9d8f2f5ddb9de95b02c8af2d38387b3370b8
RobomrFox <avikp2002@gmail.com>
2025-09-24 10:23:33 -0600

    Bug Fix: Inventory list_products

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from product import *
+from inventory import *
 
 def main():
     print("Hello from inventory-management-system!")
 
     milk = Product(1234, 'Milk', 23.49, 50)
 
+    eggs = Product(2232, 'Eggs', 10.99, 10)
+
     print(milk.get_product_details())
+
+    inventory = Inventory()
+    inventory.add_product(eggs)
+
+    print(inventory.list_products())
 
 
 if __name__ == "__main__":
